b8ab34a795734482c8e33164d025916b6ccaa379
Ben Hulan <benhulan@gmail.com>
2016-07-09 22:58:24 -0700

    adds process.env

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.get('/', function(req, res) {
 
 app.use('/api/flashcards', routes.flashcardRouter);
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log("express-heroku-starter is running on port 3000");
 });
 
