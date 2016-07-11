2781ac440d37f2129c75f3b494280434f924a992
Michael Norelli <mnorelli360@gmail.com>
2016-07-11 14:14:21 -0700

    edits package.json to add nodemon and add start code, and adds port 500 to server.js

--- models/index.js
+++ models/index.js
@@ -1,6 +1,8 @@
 var mongoose = require("mongoose");
 
-mongoose.connect( process.env.MONGOLAB_COBALT_URI || "mongodb://0.0.0.0/flashcards");
+mongoose.connect( process.env.MONGOLAB_URI ||
+                  process.env.MONGOLAB_COBALT_URI || 
+                  mongodb://heroku_b7gd64bq:16i5q92o9blpipa2h9b8152l1h@ds027628.mlab.com:27628/heroku_b7gd64bq )
 
 // After creating a new model, require and export it:
 module.exports.Flashcard = require("./flashcard.js");
