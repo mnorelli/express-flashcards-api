8663ae85996b8b8d3ecbda13c0d220b136592979
Michael Norelli <mnorelli360@gmail.com>
2016-07-11 14:28:34 -0700

    edits index.js to change localhost URL

--- models/index.js
+++ models/index.js
@@ -2,8 +2,7 @@ var mongoose = require("mongoose");
 
 mongoose.connect( process.env.MONGODB_URI ||
                   process.env.MONGOLAB_COBALT_URI || 
-                  localhost://127.0.0.1/flashcards
-                   )
+                  "http://localhost:5000/flashcards" )
 
 // After creating a new model, require and export it:
 module.exports.Flashcard = require("./flashcard.js");
