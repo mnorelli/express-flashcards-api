86ef07dd0784fc51c57bd6b3b81262ae1197b975
Michael Norelli <mnorelli360@gmail.com>
2016-07-11 14:23:22 -0700

    edits mongoose connect environment vars

--- models/index.js
+++ models/index.js
@@ -1,8 +1,8 @@
 var mongoose = require("mongoose");
 
-mongoose.connect( process.env.MONGOLAB_URI ||
+mongoose.connect( process.env.MONGODB_URI ||
                   process.env.MONGOLAB_COBALT_URI || 
-                  mongodb://heroku_b7gd64bq:16i5q92o9blpipa2h9b8152l1h@ds027628.mlab.com:27628/heroku_b7gd64bq )
+                  localhost://127.0.0.1/flashcards )
 
 // After creating a new model, require and export it:
 module.exports.Flashcard = require("./flashcard.js");
