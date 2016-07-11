758ccc548dc523d30fde1cfd7f2636623ab21a33
Ben Hulan <benhulan@gmail.com>
2016-07-10 18:06:46 -0700

    change connection settings

--- models/index.js
+++ models/index.js
@@ -1,7 +1,6 @@
 var mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGOLAB_URI || 
-    process.env.MONGOHQ_URI || "mongodb://user:pass@host:port/flashcards");
+mongoose.connect( process.env.MONGOLAB_COBALT_URI || "mongodb://0.0.0.0/flashcards");
 
 // After creating a new model, require and export it:
 module.exports.Flashcard = require("./flashcard.js");
