74f99745f12095bb68bfb96e22004157d8885615
Michael Norelli <mnorelli360@gmail.com>
2016-07-11 14:25:16 -0700

    fixes typo in server.js

--- models/index.js
+++ models/index.js
@@ -2,7 +2,8 @@ var mongoose = require("mongoose");
 
 mongoose.connect( process.env.MONGODB_URI ||
                   process.env.MONGOLAB_COBALT_URI || 
-                  localhost://127.0.0.1/flashcards )
+                  localhost://127.0.0.1/flashcards
+                   )
 
 // After creating a new model, require and export it:
 module.exports.Flashcard = require("./flashcard.js");
